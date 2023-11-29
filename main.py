5d1790c8a94bfda89527aba10368d27ca4cf31ac
Your Name <you@example.com>
2023-11-29 08:52:56 +0530

    Second Commit

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ import matplotlib.pyplot as plt
 
 # Install and import Meta AI's SAM and OWL-ViT models
 # Make sure to replace these with the actual import statements for SAM and OWL-ViT
-# You may need to install the respective libraries using pip install
 from sam import SAM
 from owl_vit import OWLViT
 
 # Function for image preprocessing
-def preprocess_image(image):
+def preprocess_image(image, target_size=(224, 224), chw_format=True):
     # Resize the image to the required input size of the model
-    target_size = (224, 224)  # Adjust this based on your model's input size
     preprocessed_image = cv2.resize(image, target_size)
 
     # Normalize the image values to be in the range [0, 1]
     preprocessed_image = preprocessed_image.astype(np.float32) / 255.0
 
     # Ensure the image is in the format expected by the model
-    # For example, if the model expects input in CHW format
-    # preprocessed_image = np.transpose(preprocessed_image, (2, 0, 1))
+    if chw_format:
+        preprocessed_image = np.transpose(preprocessed_image, (2, 0, 1))
 
     return preprocessed_image
 
@@ -36,15 +34,43 @@ def postprocess_segmentation_mask(segmentation_mask):
     return postprocessed_mask
 
 # Function for post-processing object detection results
-def postprocess_object_detection(results, confidence_threshold=0.5):
+def postprocess_object_detection(results, confidence_threshold=0.5, nms_threshold=0.3):
     # Filter results based on confidence threshold
     filtered_results = [result for result in results if result['confidence'] > confidence_threshold]
 
-    # You may perform additional post-processing steps based on your requirements
-    # For example, non-maximum suppression (NMS), sorting by confidence, etc.
-    # ...
+    # Sort results by confidence in descending order
+    sorted_results = sorted(filtered_results, key=lambda x: x['confidence'], reverse=True)
 
-    return filtered_results
+    # Apply non-maximum suppression (NMS)
+    final_results = []
+    for result in sorted_results:
+        # Check if the result overlaps significantly with any previously selected result
+        overlap = False
+        for final_result in final_results:
+            iou = calculate_intersection_over_union(result['bbox'], final_result['bbox'])
+            if iou > nms_threshold:
+                overlap = True
+                break
+
+        # If there is no significant overlap, add the result to the final results
+        if not overlap:
+            final_results.append(result)
+
+    return final_results
+
+def calculate_intersection_over_union(box1, box2):
+    # Calculate the intersection over union (IoU) of two bounding boxes
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[0] + box1[2], box2[0] + box2[2])
+    y2 = min(box1[1] + box1[3], box2[1] + box2[3])
+
+    intersection_area = max(0, x2 - x1) * max(0, y2 - y1)
+    box1_area = box1[2] * box1[3]
+    box2_area = box2[2] * box2[3]
+
+    iou = intersection_area / float(box1_area + box2_area - intersection_area)
+    return iou
 
 # Load the image
 image_path = 'path/to/your/image.jpg'
@@ -82,14 +108,16 @@ plt.title('Segmentation Mask')
 
 # Visualize post-processed object detection results on the original image
 plt.subplot(1, 3, 3)
+# Create a copy of the original image for visualization
+vis_image = image.copy()
 for result in postprocessed_results:
     x, y, w, h = result['bbox']
     label = result['label']
 
-    cv2.rectangle(image, (int(x), int(y)), (int(x + w), int(y + h)), (0, 255, 0), 2)
-    cv2.putText(image, label, (int(x), int(y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    cv2.rectangle(vis_image, (int(x), int(y)), (int(x + w), int(y + h)), (0, 255, 0), 2)
+    cv2.putText(vis_image, label, (int(x), int(y) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(vis_image, cv2.COLOR_BGR2RGB))
 plt.title('Object Detection Results')
 
 plt.show()
